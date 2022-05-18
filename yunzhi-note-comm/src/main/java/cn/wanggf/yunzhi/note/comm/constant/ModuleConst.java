146e1e728c342ffdfb791cc890c68a6619f9c19b
cnwanggf <llzero54@foxmail.com>
2022-05-18 22:00:41 +0800

    fix: 模块和服务名定义拆分

--- yunzhi-note-comm/src/main/java/cn/wanggf/yunzhi/note/comm/constant/ModuleConst.java
+++ yunzhi-note-comm/src/main/java/cn/wanggf/yunzhi/note/comm/constant/ModuleConst.java
@@ -1,7 +1,7 @@
 package cn.wanggf.yunzhi.note.comm.constant;
 
 /**
- * 模块名称（服务名称）
+ * 模块名称
  *
  * @author wanggf
  */
@@ -10,8 +10,6 @@ public interface ModuleConst {
      * 模块名称前缀
      */
     String MODULE_PREFIX = "yunzhi-note-";
-
-    /*------------------------- 项目所有模块名称 -------------------------*/
 
     /**
      * 认证模块
@@ -34,21 +32,6 @@ public interface ModuleConst {
     String MODULE_DB = MODULE_PREFIX + "db";
 
     /**
-     * Feign定制模块
-     */
-    String MODULE_FEIGN = MODULE_PREFIX + "feign";
-
-    /**
-     * 编辑模块
-     */
-    String MODULE_EDITOR = MODULE_PREFIX + "editor";
-
-    /**
-     * 文件模块
-     */
-    String MODULE_FILE = MODULE_PREFIX + "file";
-
-    /**
      * 网关
      */
     String MODULE_GATEWAY = MODULE_PREFIX + "gateway";
@@ -59,7 +42,7 @@ public interface ModuleConst {
     String MODULE_PLUGIN = MODULE_PREFIX + "plugin";
 
     /**
-     * 后台主服务
+     * 主服务
      */
     String MODULE_SERVER = MODULE_PREFIX + "server";
 
@@ -69,18 +52,22 @@ public interface ModuleConst {
     String MODULE_SERVICE = MODULE_PREFIX + "service";
 
     /**
+     * 后台管理模块
+     */
+    String MODULE_ADMIN = MODULE_PREFIX + "admin";
+
+    /**
+     * 文件模块
+     */
+    String MODULE_FILE = MODULE_PREFIX + "file";
+
+    /**
+     * 编辑模块
+     */
+    String MODULE_EDITOR = MODULE_PREFIX + "editor";
+
+    /**
      * 个人空间
      */
     String MODULE_SPACE = MODULE_PREFIX + "space";
-
-    /*------------------------- 所有服务名称 -------------------------*/
-    /**
-     * 网关服务
-     */
-    String SERVER_GATEWAY = MODULE_GATEWAY;
-
-    /**
-     * 服务支撑
-     */
-    String SERVER_SERVICE = MODULE_SERVICE;
 }
