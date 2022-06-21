7928121f7e2a49ec03bdf5a224dd709ae50664dc
cpoet <llzero54@foxmail.com>
2022-06-21 23:45:53 +0800

    fix: 用户认证

--- note-auth/src/main/java/cn/cpoet/yunzhi/note/auth/configuration/AuthenticateConfig.java
+++ note-auth/src/main/java/cn/cpoet/yunzhi/note/auth/configuration/AuthenticateConfig.java
@@ -3,9 +3,11 @@ package cn.cpoet.yunzhi.note.auth.configuration;
 import cn.cpoet.yunzhi.note.api.auth.AuthContext;
 import cn.cpoet.yunzhi.note.api.auth.Subject;
 import cn.cpoet.yunzhi.note.auth.configuration.auto.AuthenticateProperties;
+import cn.cpoet.yunzhi.note.auth.core.SimpleAuthContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
@@ -20,6 +22,7 @@ import org.springframework.context.annotation.ScopedProxyMode;
 @RequiredArgsConstructor
 public class AuthenticateConfig {
     @Bean
+    @RefreshScope
     @ConfigurationProperties(prefix = "note.auth")
     public AuthenticateProperties authenticateProperties() {
         return new AuthenticateProperties();
@@ -27,8 +30,8 @@ public class AuthenticateConfig {
 
     @Bean
     @ConditionalOnMissingBean
-    public AuthContext authContext(AuthenticateProperties authenticateProperties) {
-        return null;
+    public AuthContext authContext() {
+        return new SimpleAuthContext();
     }
 
     @Bean
