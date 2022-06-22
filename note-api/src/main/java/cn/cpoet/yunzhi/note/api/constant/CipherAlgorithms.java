3b056636686354ad7a4d37ff6b82cf970021e4c5
cpoet <llzero54@foxmail.com>
2022-06-22 19:54:00 +0800

    feat: 系统内置算法类型

---
+++ note-api/src/main/java/cn/cpoet/yunzhi/note/api/constant/CipherAlgorithms.java
@@ -0,0 +1,23 @@
+package cn.cpoet.yunzhi.note.api.constant;
+
+/**
+ * 系统选用的加密算法
+ *
+ * @author CPoet
+ */
+public enum CipherAlgorithms {
+    /**
+     * 未指定
+     */
+    NONE,
+
+    /**
+     * RSA非对称算法
+     */
+    RSA,
+
+    /**
+     * DES对称算法
+     */
+    DES
+}
