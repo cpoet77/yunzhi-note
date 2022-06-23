1153f9e115d19595744075d862f481ee8fbf7f07
cpoet <llzero54@foxmail.com>
2022-06-24 00:14:05 +0800

    fix: API信息

--- note-web/note-web-comm/src/main/java/cn/cpoet/yunzhi/note/web/comm/controller/RouterController.java
+++ note-web/note-web-comm/src/main/java/cn/cpoet/yunzhi/note/web/comm/controller/RouterController.java
@@ -4,6 +4,8 @@ import cn.cpoet.yunzhi.note.comm.annotation.FeignTarget;
 import cn.cpoet.yunzhi.note.domain.dao.RouterDao;
 import cn.cpoet.yunzhi.note.domain.model.Router;
 import cn.cpoet.yunzhi.note.web.comm.service.RouterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/router")
 @RequiredArgsConstructor
+@Tag(name = "路由信息")
 public class RouterController {
     private final RouterService routerService;
 
     @FeignTarget
     @GetMapping("/list")
+    @Operation(summary = "获取有效路由列表")
     public List<Router> list() {
         return routerService.list();
     }
