0abd2e74e69a160a7eacd406b952e4e419a092d8
CPoet <llzero54@foxmail.com>
2022-07-22 17:13:28 +0800

    fix: 开启删除

--- note-web/note-web-comm/src/main/java/cn/cpoet/yunzhi/note/web/comm/CommApplication.java
+++ note-web/note-web-comm/src/main/java/cn/cpoet/yunzhi/note/web/comm/CommApplication.java
@@ -3,10 +3,12 @@ package cn.cpoet.yunzhi.note.web.comm;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author CPoet
  */
+@EnableCaching
 @SpringBootApplication
 public class CommApplication {
     public static void main(String[] args) {
